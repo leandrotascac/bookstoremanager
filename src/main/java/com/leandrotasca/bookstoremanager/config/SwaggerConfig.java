bb8e69e2bc417e6f4a04932a20f824bcbf48eab0
Leandro <Leandro@Leandros-MacBook-Pro.local>
2022-03-22 18:58:23 -0300

    Segundo Commit

--- src/main/java/com/leandrotasca/bookstoremanager/config/SwaggerConfig.java
+++ src/main/java/com/leandrotasca/bookstoremanager/config/SwaggerConfig.java
@@ -1,8 +1,59 @@
 package com.leandrotasca.bookstoremanager.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @Configuration
 @EnableSwagger2
-public class SeaggerConfig {
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    private static final String BASE_PACKAGE = "com.leandrotasca.bookstoremanager";
+    private static final String API_TITLE = "Book Store Manager";
+    private static final String API_DESCRIPTION = "Bookstore Manager API Professional";
+    private static final String API_VERSION = "1.0.0";
+    private static final String CONTACT_NAME = "Leandro Cardoso";
+    private static final String CONTACT_GITHUB = "https://github.com/leandrotascac";
+    private static final String CONTACT_EMAIL = "leandrotascac@gmail.com";
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(BASE_PACKAGE))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(buildApiInfo());
+    }
+
+    private ApiInfo buildApiInfo() {
+        return new ApiInfoBuilder()
+                .title(API_TITLE)
+                .description(API_DESCRIPTION)
+                .version(API_VERSION)
+                .contact(new Contact(CONTACT_NAME,  CONTACT_GITHUB, CONTACT_EMAIL))
+                .build();
+    }
+
+
 }
